c0f7048f54601b326fbd8144b801a5dba5081fa3
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2012-11-05 18:54:01 +0100

    Document that pop() might return Null

--- lib/Kue/Queue.php
+++ lib/Kue/Queue.php
@@ -11,9 +11,10 @@ interface Queue
 {
     /**
      * Blocks until a job is available, and returns it. This is used by the
-     * worker script that's shipped with Spark.
+     * worker script that's shipped with Kue.
      *
-     * @return Job
+     * @return Job|null Returns either a Job, or Null when the operation 
+     * timed out.
      */
     function pop();
 
@@ -29,8 +30,9 @@ interface Queue
      * Flushes the queue.
      *
      * This is mainly useful for queues which are clients to a 3rd party system
-     * which stores the jobs. This method is called after the response has been sent
-     * to the client, and can be used to send all pushed jobs in one go.
+     * which stores the jobs. This method should be called after the response has been sent
+     * to the client, and can be used to send all pushed jobs in one go 
+     * without much notice of the user.
      *
      * @return void
      */
